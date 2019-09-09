050bf5d63024c7c8b555b13702e78f7fd2fdab61
Gitti <gitti.akihisa.akihisa@gmail.com>
2019-09-09 22:27:49 +0900

    コミット

--- sangeki/kifu_csv.php
+++ sangeki/kifu_csv.php
@@ -6,5 +6,5 @@ echo json_encode(array(
     'day' => $_POST['day'],
     'chara' => $_POST['chara'],
     'action' => json_decode($_POST['action']),
-    'memo' => json_decode($_POST['memo']),
+    'memo' => $_POST['memo'],
 ));
