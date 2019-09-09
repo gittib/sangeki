d64b6cb1aad35ad75eec4c16223a291054428746
Gitti <gitti.akihisa.akihisa@gmail.com>
2019-09-09 21:59:51 +0900

    コミット

--- sangeki/kifu_csv.php
+++ sangeki/kifu_csv.php
@@ -1,5 +1,6 @@
 <?
 require_once('../secret/common.php');
+header('content-type: application/json; charset=utf-8');
 echo json_encode(array(
     'loop' => $_POST['loop'],
     'day' => $_POST['day'],
