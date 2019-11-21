319dc79f872e4018989e3bc68b511956fc918414
Takahashi Akihisa <takahashi@spread1.co.jp>
2019-11-21 20:03:58 +0900

    一旦配役

--- secret/kyakuhon_list/4901.php
+++ secret/kyakuhon_list/4901.php
@@ -9,32 +9,32 @@ $oSangeki = (object)array(
     'rule' => array(
         '高貴なる血族',
         '魔女の呪い',
-        '恐慌と妄執と',
+        '恋愛風景',
     ),
     'special_rule' => "巫女は少女でなく少年として扱う。",
     'loop' => 5,
     'day' => 5,
     'character' => array(
         '巫女' => array(
-            'role' => 'ウィッチ',
+            'role' => 'ヴァンパイア',
         ),
         '異世界人' => array(
             'role' => 'ミスリーダー',
         ),
         '軍人' => array(
         ),
-        '大物' => array(
-            'role' => 'ヴァンパイア',
-            'note' => '学校',
+        'A.I.' => array(
+            'role' => 'メインラバーズ',
         ),
         '情報屋' => array(
+            'role' => 'ウィッチ',
         ),
         'アイドル' => array(
+            'role' => 'ラバーズ',
         ),
         '女の子' => array(
         ),
         '委員長' => array(
-            'role' => 'キーパーソン',
         ),
         '女子学生' => array(
             'role' => 'キーパーソン',
@@ -45,9 +45,17 @@ $oSangeki = (object)array(
             'name' => '呪いの目覚め',
             'criminal' => '学校の群像',
         ),
+        3 => array(
+            'name' => '噂の御呪い',
+            'criminal' => 'アイドル',
+        ),
+        5 => array(
+            'name' => '行方不明',
+            'criminal' => '女子学生',
+        ),
     ),
     'advice' => (object)array(
-        'notice' => '',
+        'notice' => 'プロモーションカード「女の子」を使用します。',
         'summary' => "",
         'detail' => "",
         'template' => array(
@@ -55,6 +63,7 @@ $oSangeki = (object)array(
                 1 => array(
                     '女子学生' => '暗躍+2',
                     '学校' => '暗躍+1',
+                    'アイドル' => '不安+1',
                 ),
             ),
         ),
