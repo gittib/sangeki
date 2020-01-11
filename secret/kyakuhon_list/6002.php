6fda7d6b724de4640acd7e033541136a6b29041a
Gitti <gitti.akihisa.akihisa@gmail.com>
2020-01-11 15:37:02 +0900

    一旦コミット

---
+++ secret/kyakuhon_list/6002.php
@@ -0,0 +1,87 @@
+<?
+
+$oSangeki = (object)array(
+    'title' => '',
+    'secret' => true,
+    'writer' => 'ペンスキー',
+    'difficulity' => 5,
+    'set' => 'UM',
+    'rule' => array(
+        '絶望のカウントダウン',
+        '問題児の苦悩',
+        '',
+    ),
+    'loop' => 4,
+    'day' => 5,
+    'character' => array(
+        '神格' => array(
+            'note' => '2ループ目',
+        ),
+        '巫女' => array(
+            'role' => 'フレンド',
+        ),
+        '異世界人' => array(
+            'role' => 'イレイザー',
+        ),
+        '軍人' => array(
+        ),
+        '入院患者' => array(
+            'role' => 'ミスリーダー',
+        ),
+        '大物' => array(
+            'role' => 'センドウシャ',
+            'note' => '病院',
+        ),
+        '教師' => array(
+        ),
+        'お嬢様' => array(
+        ),
+        '手先' => array(
+        ),
+    ),
+    'incident' => array(
+        1 => array(
+            'name' => '遂行者',
+            'criminal' => '軍人',
+        ),
+        2 => array(
+            'name' => '怨嗟の雄叫び',
+            'criminal' => '入院患者',
+        ),
+    ),
+    'advice' => (object)array(
+        'notice' => "",
+        'summary' => "",
+        'detail' => "",
+        'victoryConditions' => array(
+            array(
+                'condition' => '主人公の殺害',
+                'way' => array(
+                    'イレイザーの能力',
+                ),
+            ),
+            array(
+                'condition' => '絶望のカウントダウン',
+                'way' => array(
+                    'ループ終了時にイレイザーの初期エリアに暗躍カウンターがX個ある(X=3-Exゲージ)',
+                ),
+            ),
+            array(
+                'condition' => 'フレンドの殺害',
+                'way' => array(
+                    '遂行者',
+                ),
+            ),
+        ),
+        'template' => array(
+            '基本' => array(
+                0 => '手先は神社に配置',
+                1 => array(
+                    '神社' => '暗躍+1',
+                    '手先' => '不安+1',
+                    '入院患者' => '不安+1',
+                ),
+            ),
+        ),
+    ),
+);
