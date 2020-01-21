24d39bffd4ed7bfc84e3007aa1c9db2233c689d5
Takahashi Akihisa <takahashi@spread1.co.jp>
2020-01-21 15:43:48 +0900

    コミット

--- secret/kyakuhon_list/1007.php
+++ secret/kyakuhon_list/1007.php
@@ -3,25 +3,39 @@
 $oSangeki = (object)array(
     'title' => '',
     'writer' => 'ペンスキー',
-    'difficulity' => 5,
+    'difficulity' => 3,
     'set' => 'BTX',
     'rule' => array(
         '僕と契約しようよ！',
         '友情サークル',
-        '因果の糸',
+        '不定因子χ',
     ),
-    'loop' => 5,
-    'day' => 6,
+    'special_rule' => '脚本家は友好禁止、移動斜め、暗躍+2を使えない',
+    'loop' => 4,
+    'day' => 5,
     'character' => array(
-        '巫女' => array(
-            'role' => 'キーパーソン',
+        '黒猫' => array(
         ),
-        '異世界人' => array(
+        '軍人' => array(
+            'role' => 'フレンド',
+        ),
+        '入院患者' => array(
+            'role' => 'ファクター',
+        ),
+        '大物' => array(
+            'note' => '神社',
+        ),
+        '情報屋' => array(
+        ),
+        'A.I.' => array(
+            'role' => 'ミスリーダー',
         ),
         '手先' => array(
             'role' => 'フレンド',
+            'initPos' => '都市',
         ),
         'お嬢様' => array(
+            'role' => 'キーパーソン',
         ),
     ),
     'incident' => array(
@@ -30,16 +44,16 @@ $oSangeki = (object)array(
             'criminal' => '手先',
         ),
         2 => array(
-            'name' => '行方不明',
-            'criminal' => 'キーパーソン',
-        ),
-        3 => array(
-            'name' => '病院の事件',
-            'criminal' => '',
-        ),
-        6 => array(
             'name' => '流布',
-            'criminal' => 'フレンド',
+            'criminal' => '黒猫',
+        ),
+        4 => array(
+            'name' => '行方不明',
+            'criminal' => 'お嬢様',
+        ),
+        5 => array(
+            'name' => '病院の事件',
+            'criminal' => '軍人',
         ),
     ),
     'advice' => (object)array(
