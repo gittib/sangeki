ebe5fc7494d898f2f2f42e7f68c518aec2dde96e
Gitti <gitti.akihisa.akihisa@gmail.com>
2020-02-04 08:15:45 +0900

    コミット

--- secret/sangeki_header.php
+++ secret/sangeki_header.php
@@ -5,7 +5,7 @@
         <label id="nav-open" for="nav-input"><span></span></label>
         <label class="nav-unshown" id="nav-close" for="nav-input"></label>
         <div id="nav-content">
-            <ul>
+            <ul class="top_menu">
                 <li><a href="<?= TOP_PATH ?>">TOP</a></li>
                 <li><a href="<?= TOP_PATH ?>kyakuhon/">脚本リスト</a></li>
                 <li><a href="<?= TOP_PATH ?>kifu/">棋譜記録機能</a></li>
