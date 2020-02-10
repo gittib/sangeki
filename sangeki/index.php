63036772d56deb39f4a6c8bd14289b7324171cc3
Gitti <gitti.akihisa.akihisa@gmail.com>
2020-02-11 01:41:39 +0900

    コミット

--- sangeki/index.php
+++ sangeki/index.php
@@ -2,6 +2,29 @@
 define('SECRET_DIR', '../secret/');
 require_once(SECRET_DIR.'common.php');
 
+$aMenu = array(
+    (object)array(
+        'href' => 'kyakuhon/',
+        'link' => '脚本リスト',
+        'img_src' => 'images/kyakuhonka.png',
+        'img_alt' => '惨劇 脚本家',
+        'text' => "主にペンスキーの考えた自作脚本が転がってます。一部寄稿いただいた脚本もあります。紙の用意がいらないので、ペンスキーの脚本はほぼ全てここに集約されてる模様。",
+    ),
+    (object)array(
+        'href' => 'kifu/',
+        'link' => '棋譜記録機能',
+        'img_src' => 'images/heros.png',
+        'img_alt' => '主人公たち',
+        'text' => "各ループ各ターン、脚本家がどこにどのカードを置いたかは、時に推理を進めるための重要な情報となります。\n紙とペンがあるならそっちの方が良いとは思いますが、お気に召したら嬉しい限りです。",
+    ),
+    (object)array(
+        'href' => 'column/',
+        'link' => '脚本アイディア',
+        'img_src' => 'images/uzai.png',
+        'img_alt' => '脚本を彩る名優たち',
+        'text' => "ペンスキー流の脚本の書き方とか、関連アイディアの投棄場。脚本リストに挙がっているものの一部で実際に使われているアイディアもあります。\n惨劇RoopeRの脚本書くの楽しいよ！",
+    ),
+)
 ?>
 <html>
 <head>
@@ -18,21 +41,15 @@ require_once(SECRET_DIR.'common.php');
         <a href="http://bakafire.main.jp/rooper/sr_top.htm">惨劇RoopeRってなんぞ？という方は公式サイトをどうぞ</a>
     </div>
     <dl>
-        <dt><a href="kyakuhon/">
-            脚本リスト
-            <img src="<?= TOP_PATH ?>images/kyakuhonka.png" alt="惨劇 脚本家">
+        <? foreach ($aMenu as $val): ?>
+        <dt><a href="<?= e($val->href) ?>">
+            <?= e($val->link) ?>
+            <div>
+                <img src="<?= TOP_PATH . e($val->img_src) ?>" alt="<?= e($val->img_alt) ?>">
+            </div>
         </a></dt>
-        <dd>主にペンスキーの考えた自作脚本が転がってます。一部寄稿いただいた脚本もあります。紙の用意がいらないので、ペンスキーの脚本はほぼ全てここに集約されてる模様。</dd>
-        <dt><a href="kifu/">
-            棋譜記録機能
-            <img src="<?= TOP_PATH ?>images/heros.png" alt="主人公たち">
-        </a></dt>
-        <dd>各ループ各ターン、脚本家がどこにどのカードを置いたかは、時に推理を進めるための重要な情報となります。<br>紙とペンがあるならそっちの方が良いとは思いますが、お気に召したら嬉しい限りです。</dd>
-        <dt><a href="column/">
-            駄文
-            <img src="<?= TOP_PATH ?>images/uzai.png" alt="脚本を彩る名優たち">
-        </a></dt>
-        <dd>ペンスキー流の脚本の書き方とか、関連アイディアの投棄場。脚本リストに挙がっているものの一部で実際に使われているアイディアもあります。<br>惨劇RoopeRの脚本書くの楽しいよ！</dd>
+        <dd><?= nl2br(e($val->text)) ?></dd>
+        <? endforeach; ?>
     </dl>
 <?php require(SECRET_DIR.'sangeki_footer.php') ?>
 </body>
