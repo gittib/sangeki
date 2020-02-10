28740cf4d958d73bf200b92c2a7ebf55f6c0c370
Gitti <gitti.akihisa.akihisa@gmail.com>
2020-02-11 08:44:48 +0900

    コミット

--- secret/common.php
+++ secret/common.php
@@ -47,3 +47,28 @@ function difficulityName($difficulity) {
             return '悪夢';
     }
 }
+
+
+$aTopMenu = array(
+    (object)array(
+        'href' => 'kyakuhon/',
+        'link' => '脚本リスト',
+        'img_src' => 'images/kyakuhonka.png',
+        'img_alt' => '惨劇 脚本家',
+        'text' => "主にペンスキーの考えた自作脚本が転がってます。一部寄稿いただいた脚本もあります。紙の用意がいらないので、ペンスキーの脚本はほぼ全てここに集約されてる模様。",
+    ),
+    (object)array(
+        'href' => 'kifu/',
+        'link' => '棋譜記録機能',
+        'img_src' => 'images/heros.png',
+        'img_alt' => '主人公たち',
+        'text' => "各ループ各ターン、脚本家がどこにどのカードを置いたかは、時に推理を進めるための重要な情報となります。\n紙とペンがあるならそっちの方が良いとは思いますが、お気に召したら嬉しい限りです。",
+    ),
+    (object)array(
+        'href' => 'column/',
+        'link' => '脚本アイディア',
+        'img_src' => 'images/uzai.png',
+        'img_alt' => '脚本を彩る名優たち',
+        'text' => "ペンスキー流の脚本の書き方とか、関連アイディアの投棄場。脚本リストに挙がっているものの一部で実際に使われているアイディアもあります。\n惨劇RoopeRの脚本書くの楽しいよ！",
+    ),
+);
