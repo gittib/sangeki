df3e72eda391f7c37a329388e5f7b6b24c49dcaa
gitti <gitti.akihisa.akihisa@gmail.com>
2021-05-22 22:33:03 +0900

    route.phpでparse_url()を使う

--- secret/route.php
+++ secret/route.php
@@ -1,8 +1,10 @@
 <?php
 require_once(__DIR__ . '/common.php');
 
-$path = preg_replace(';\?.*$;', '', $_SERVER['REQUEST_URI']);
-if ($path == TOP_PATH) {
+$url = (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+$aUrl = parse_url($url);
+$aUrl['segments'] = explode('/', trim($aUrl['path'], '/ '));
+if ($aUrl['path'] == TOP_PATH) {
     require(PUBLIC_DIR.'top.php');
 } else {
     abort();
