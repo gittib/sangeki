15b075f198bfbdac927eabe37d788a8d80c62bbe
gitti <gitti.akihisa.akihisa@gmail.com>
2021-07-24 15:19:51 +0900

    コミット

--- secret/route.php
+++ secret/route.php
@@ -7,7 +7,7 @@ $aUrl['segments'] = explode('/', trim($aUrl['path'], '/ '));
 if ($aUrl['path'] == TOP_PATH) {
     require(PUBLIC_DIR.'top.php');
 } else if ($aUrl['path'] == TOP_PATH.'s.apk') {
-    $url = sprintf('%s://%s/%s', schema(), $_SERVER["HTTP_HOST"], TOP_PATH.'apk.php');
+    $url = sprintf('%s://%s%s', schema(), $_SERVER["HTTP_HOST"], TOP_PATH.'apk.php');
     header('Location: '.$url, true, 301);
 } else {
     abort();
