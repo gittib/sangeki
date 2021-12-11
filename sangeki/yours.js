6989f0958987cadca082817cec8a6b031eef5837
gitti <gitti.akihisa.akihisa@gmail.com>
2021-12-11 17:53:14 +0900

    コミット

--- sangeki/yours.js
+++ sangeki/yours.js
@@ -1,10 +1,12 @@
 if ($('body').hasClass('your_kyakuhon_list')) {
+    console.log('自作脚本リスト');
+
     // 自作脚本リストの処理
     JSON.parse(localStorage.getItem('scenarioList') || '[]').forEach((item) => {
         let $dom = $('#clone_base-kyakuhon_column');
         $dom.removeAttr('id');
-        $dom.removeAttr('style');
         $dom.find('a.link').attr('href', './detail.php?id='+item.id);
+        $dom.find('.title').text(item.title);
         $dom.find('.loop > strong').text(item.loop);
         $dom.find('.day > strong').text(item.day);
         $('#kyakuhon_list').append($dom);
