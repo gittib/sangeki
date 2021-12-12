d3aad9ac60569f8338fd5b9d66e9fdc2ad0893bc
gitti <gitti.akihisa.akihisa@gmail.com>
2021-12-12 17:13:45 +0900

    キャラの備考欄にプレースホルダを設定

--- sangeki/kyakuhon/yours/edit.php
+++ sangeki/kyakuhon/yours/edit.php
@@ -151,7 +151,7 @@ $aChara = [
                     <option><?= $role ?></option>
                     <?php endforeach; ?>
                 </select></span>
-                <span><input type="text" name="chara_note"></span>
+                <span><input type="text" name="chara_note" placeholder="備考欄(大物のテリトリー等)"></span>
                 <span><button class="delete">削除</button></span>
             </li>
             <li id="clone_base-incident_row" class="incident_row">
