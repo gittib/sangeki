d312330a582f9c911de983ca8b742d9ab94d743e
gitti <gitti.akihisa.akihisa@gmail.com>
2021-12-18 10:38:38 +0900

    閉じタグ修正

--- sangeki/kyakuhon/yours/index.php
+++ sangeki/kyakuhon/yours/index.php
@@ -33,7 +33,7 @@ require_once(__DIR__.'/../../../secret/common.php');
             <a href="javascript:void(0)" class="open_export_console">脚本データの引き継ぎ・バックアップ</a>
             <div class="export_console" style="display:none;">
                 <p>このボタンを押すと、脚本リストをファイルとして保存できます。</p>
-                <button class="save_as">脚本データをファイル保存</div>
+                <button class="save_as">脚本データをファイル保存</button>
                 <p>以下のファイル選択欄に<span class="download_file_name"></span>を入力することで、脚本データをこのブラウザへ追加できます。</p>
                 <input type="file" class="add_scenario_from_file">
             </div>
