11b3c1241f85cd22f4d76ae3b2e2ab9be0c8ea43
gitti <gitti.akihisa.akihisa@gmail.com>
2022-01-05 13:03:25 +0900

    route.php見づらかったのでコメント追記

--- secret/route.php
+++ secret/route.php
@@ -4,12 +4,19 @@ require_once(__DIR__ . '/common.php');
 $url = schema() . '://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 $aUrl = parse_url($url);
 $aUrl['segments'] = explode('/', trim($aUrl['path'], '/ '));
+
+
 if ($aUrl['path'] == TOP_PATH) {
+    // TOPページ
     require(PUBLIC_DIR.'top.php');
+
 } else if ($aUrl['path'] == TOP_PATH.'s.apk') {
+    // APK直接配ってた時のDL用URLは、Google Playへの案内ページにリダイレクトする
     $url = sprintf('%s://%s%s', schema(), $_SERVER["HTTP_HOST"], TOP_PATH.'apk.php');
     header('Location: '.$url, true, 301);
+
 } else if (($aUrl['segments'][1] ?? '') == 'zakkan') {
+    // 雑感ページ
     $ymd = $aUrl['segments'][2] ?? '';
     $path = SECRET_DIR."zakkan/${ymd}.php";
     if (empty($ymd)) {
@@ -19,6 +26,7 @@ if ($aUrl['path'] == TOP_PATH) {
     } else {
         abort();
     }
+
 } else {
     abort();
 }
