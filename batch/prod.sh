ccbe2a5d510e9dfd6831b4cd7f1a8a2c0b94dbdc
gitti <gitti.akihisa.akihisa@gmail.com>
2022-01-05 16:23:52 +0900

    define.js追加

--- batch/prod.sh
+++ batch/prod.sh
@@ -18,7 +18,7 @@ ssh-agent -k
 chmod 777 secret/cache
 git log -n 1 --pretty=%H > secret/cache/latest_git_hash
 
-cp /dev/null sangeki/all.js
+cat secret/assets/js/define.js > sangeki/all.js
 find secret/assets/js/functions/ -type f | grep js | xargs cat >> sangeki/all.js
 find secret/assets/js/individual/ -type f | grep js | xargs cat >> sangeki/all.js
 
