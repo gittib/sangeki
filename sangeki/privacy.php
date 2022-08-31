50e69e03f1ee3ed1d4bd20a22413f77b7cb23483
gitti <gitti.akihisa.akihisa@gmail.com>
2022-08-31 12:33:22 +0900

    CSS調整

--- sangeki/privacy.php
+++ sangeki/privacy.php
@@ -18,7 +18,7 @@ require(SECRET_DIR.'top_menu.php');
         <li>本アプリでは、利用者の個人情報は特に求められません。</li>
         <li>利用者が入力したテキスト情報をアプリ内に保存する機能はありますが、保存された情報がネットワークにアップロードされることはありません。また、個人情報やそれに類する情報の入力が想定される入力欄は存在しません。</li>
         <li>Push通知や広告なども一切配信されないため、端末を特定する情報も収集、保持することはありません。</li>
-        <li>本アプリについて、BakaFire Partyへ問い合わせる事はできません。本アプリについての問い合わせは、開発者である<a href="https://twitter.com/gittib_gittib">ペンスキー</a>までお願いいたします。</li>
+        <li>本アプリについての問い合わせは、開発者である<a href="https://twitter.com/gittib_gittib">ペンスキー</a>までお願いいたします。<br>BakaFire Partyへ、本アプリについて問い合わせる事はできません。</li>
     </ol>
 <?php require(SECRET_DIR.'sangeki_footer.php') ?>
 </body>
